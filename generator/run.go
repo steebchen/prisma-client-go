7e2b90e3c8416e483915be8e0e66d9e3d3f47181
Luca Steeb <contact@luca-steeb.com>
2019-10-18 19:06:17 +0200

    feat(generator): range over multiple templates

--- generator/run.go
+++ generator/run.go
@@ -13,37 +13,38 @@ import (
 )
 
 func addDefaults(input *Root) {
-	fmt.Printf("package: %s\n", input.Generator.Config.Package)
-	fmt.Printf("file: %s\n", input.Generator.Output)
-	input.Generator.Config.Package = "main"
+	if input.Generator.Config.Package == "" {
+		input.Generator.Config.Package = "main"
+	}
 }
 
 func Run(input Root) error {
 	addDefaults(&input)
 
-	asset, err := templates.Asset("generator/templates/main.gotpl")
-	if err != nil {
-		return errors.Wrap(err, "could not get main template asset")
-	}
-
-	tpl, err := template.New("main").Parse(string(asset))
-	if err != nil {
-		return errors.Wrap(err, "could not parse templates")
-	}
-
 	var buf bytes.Buffer
-	err = tpl.Execute(&buf, input)
-	if err != nil {
+
+	for _, name := range templates.AssetNames() {
+		asset, err := templates.Asset(name)
+		if err != nil {
+			return errors.Wrap(err, "could not get main template asset")
+		}
+
+		tpl, err := template.New(name).Parse(string(asset))
+		if err != nil {
+			return errors.Wrap(err, "could not parse templates")
+		}
+
+		err = tpl.Execute(&buf, input)
+		if err != nil {
+			return errors.Wrap(err, fmt.Sprintf("could not write template data to file writer %s", input.Generator.Output))
+		}
+	}
+
+	if err := ioutil.WriteFile(input.Generator.Output, buf.Bytes(), 0644); err != nil {
 		return errors.Wrap(err, fmt.Sprintf("could not write template data to file writer %s", input.Generator.Output))
 	}
 
-	err = ioutil.WriteFile(input.Generator.Output, buf.Bytes(), 0644)
-	if err != nil {
-		return errors.Wrap(err, fmt.Sprintf("could not write template data to file writer %s", input.Generator.Output))
-	}
-
-	err = exec.Command("go", "fmt", input.Generator.Output).Run()
-	if err != nil {
+	if err := exec.Command("go", "fmt", input.Generator.Output).Run(); err != nil {
 		return errors.Wrap(err, fmt.Sprintf("could not format file with go fmt %s", input.Generator.Output))
 	}
 
