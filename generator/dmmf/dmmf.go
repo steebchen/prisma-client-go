d3f6cba29f224747e567d3eba3ab72994feadb23
Luca Steeb <contact@luca-steeb.com>
2019-11-08 11:19:09 +0100

    feat(query/relations): implement basic relations

--- generator/dmmf/dmmf.go
+++ generator/dmmf/dmmf.go
@@ -97,6 +97,87 @@ func (Document) Actions() []Action {
 	}
 }
 
+// Method defines the method for the virtual types method
+type Method struct {
+	Name   string
+	Action string
+}
+
+// Type defines the data struct for the virtual types method
+type Type struct {
+	Name    string
+	Methods []Method
+}
+
+// Types provides virtual types and their actions
+func (Document) Types() []Type {
+	number := []Method{{
+		Name:   "Equals",
+		Action: "",
+	}, {
+		Name:   "LT",
+		Action: "lt",
+	}, {
+		Name:   "GT",
+		Action: "gt",
+	}, {
+		Name:   "LTE",
+		Action: "lte",
+	}, {
+		Name:   "GTE",
+		Action: "gte",
+	}}
+	return []Type{{
+		Name: "String",
+		Methods: []Method{{
+			Name:   "Equals",
+			Action: "",
+		}, {
+			Name:   "Contains",
+			Action: "contains",
+		}, {
+			Name:   "HasPrefix",
+			Action: "starts_with",
+		}, {
+			Name:   "HasSuffix",
+			Action: "ends_with",
+		}},
+	}, {
+		Name: "Boolean",
+		Methods: []Method{{
+			Name:   "Equals",
+			Action: "",
+		}, {
+			Name:   "AfterEquals",
+			Action: "gte",
+		}},
+	}, {
+		Name:    "Int",
+		Methods: number,
+	}, {
+		Name:    "Float",
+		Methods: number,
+	}, {
+		Name: "DateTime",
+		Methods: []Method{{
+			Name:   "Equals",
+			Action: "",
+		}, {
+			Name:   "Before",
+			Action: "lt",
+		}, {
+			Name:   "After",
+			Action: "gt",
+		}, {
+			Name:   "BeforeEquals",
+			Action: "lte",
+		}, {
+			Name:   "AfterEquals",
+			Action: "gte",
+		}},
+	}}
+}
+
 // Enum describes an enumerated type.
 type Enum struct {
 	Name   types.String   `json:"name"`
