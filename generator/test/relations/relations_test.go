42a6cf040a80cb52a6bc9b338e04aaa337f97e4d
Luca Steeb <contact@luca-steeb.com>
2019-11-08 15:34:58 +0100

    stuff [wip]

--- generator/test/relations/relations_test.go
+++ generator/test/relations/relations_test.go
@@ -93,6 +93,119 @@ func TestRelations(t *testing.T) {
 
 			assert.Equal(t, expected, actual)
 		},
+	}, {
+		name: "find many posts by user",
+		// language=GraphQL
+		before: `
+			mutation {
+				unrelated: createOnePost(data: {
+					id: "nope",
+					title: "nope",
+					content: "nope",
+					user: {
+						create: {
+							id: "unrelated",
+							email: "unrelated",
+							username: "unrelated",
+							name: "unrelated",
+						}
+					}
+				}) {
+					id
+				}
+
+				user: createOneUser(data: {
+					id: "relations",
+					email: "john@example.com",
+					username: "johndoe",
+					name: "John",
+					posts: {
+						create: [{
+							id: "a",
+							title: "common",
+							content: "a",
+						}, {
+							id: "b",
+							title: "common",
+							content: "b",
+						}],
+					},
+				}) {
+					id
+				}
+			}
+		`,
+		run: func(t *testing.T, client Client, ctx cx) {
+			actual, err := client.User.FindMany(
+				User.Email.Equals("john@example.com"),
+				// Post.Content.Equals("f"), // should fail
+				User.Posts().Title.Equals("common"),
+				User.Posts().User().Email.Equals("common"),
+				User.Posts().User().Posts().Content.Equals("common"),
+				User.Posts().User().Posts().User().Email.Equals("common"),
+				User.Comments().User().Posts().Title.Equals("common"),
+				User.Comments().User().Posts().Title.Equals("common"),
+			).Exec(ctx)
+			if err != nil {
+				t.Fatalf("fail %s", err)
+			}
+
+			p, err := client.Post.FindMany(
+				// User.Email.Equals("john@example.com"), // should fail
+				Post.Title.Equals("common"),
+				Post.User().Email.Equals("common"),
+				Post.User().Posts().Content.Equals("common"),
+				Post.User().Posts().User().Email.Equals("common"),
+				Post.Comments().Post().Title.Equals("common"),
+				Post.Comments().Post().User().Email.Equals("common"),
+				Post.Comments().Post().Comments().Content.Equals("common"),
+				Post.Comments().User().Posts().Title.Equals("common"),
+				Post.Comments().User().Posts().Title.Equals("common"),
+			).Exec(ctx)
+			if err != nil {
+				t.Fatalf("fail %s", err)
+			}
+
+			Post.User()
+			Post.Comments()
+
+			User.Posts()
+			User.Comments()
+
+			Comment.User()
+			Comment.Post()
+
+			Post.User().Email.Equals("f")
+			Post.Comments().Content.Equals("f")
+			Post.User(). /*Comments()*/ Email.Equals("f")
+			Post.User().Posts().Title.Equals("f")
+			Post.User().Posts().User().Email.Equals("f")
+			Post.User().Posts().User().Posts().User().Email.Equals("f")
+			Post.User().Posts().User().Posts().Content.Equals("f")
+
+			Post.User().Posts().Comments()
+			Post.Comments().Post()
+
+			Post.Comments().Content.Equals("f")
+			Post.Comments().Post().Content.Equals("f")
+			Post.Comments().Post().Comments().Post().Comments().Post().Comments().Content.Equals("f")
+
+			expected := []PostModel{{
+				post{
+					ID:      "a",
+					Title:   "common",
+					Content: str("a"),
+				},
+			}, {
+				post{
+					ID:      "b",
+					Title:   "common",
+					Content: str("b"),
+				},
+			}}
+
+			assert.Equal(t, expected, actual)
+		},
 	}}
 	for _, tt := range tests {
 		tt := tt
