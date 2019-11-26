72e4d69092949944491362f8d9b78964abb3ecb3
Luca Steeb <contact@luca-steeb.com>
2019-11-26 14:17:32 +0100

    test(integration): ignore signal interrupt error

--- generator/test/basic/basic_test.go
+++ generator/test/basic/basic_test.go
@@ -508,11 +508,10 @@ func TestBasic(t *testing.T) {
 			}
 
 			defer func() {
-				_ = client.Disconnect()
-				// TODO blocked by prisma-engine panicking on disconnect
-				// if err != nil {
-				// 	t.Fatalf("could not disconnect %s", err)
-				// }
+				err := client.Disconnect()
+				if err != nil {
+					t.Fatalf("could not disconnect: %s", err)
+				}
 			}()
 
 			ctx := context.Background()
