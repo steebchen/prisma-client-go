e7eb7627d3322e3e4b9dd4b6a59992c9c3ed4045
Luca Steeb <contact@luca-steeb.com>
2019-11-26 13:27:51 +0100

    feat(generator): implement OrderBy

---
+++ generator/test/pagination/pagination_test.go
@@ -0,0 +1,180 @@
+package pagination
+
+//go:generate prisma2 generate
+
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/prisma/photongo/generator/test/hooks"
+)
+
+type cx = context.Context
+type Func func(t *testing.T, client Client, ctx cx)
+
+func TestPagination(t *testing.T) {
+	t.Parallel()
+
+	tests := []struct {
+		name   string
+		before string
+		run    Func
+	}{{
+		name: "order by ASC",
+		// language=GraphQL
+		before: `
+			mutation {
+				a: createOnePost(data: {
+					id: "a",
+					title: "a",
+					content: "a",
+				}) {
+					id
+				}
+
+				c: createOnePost(data: {
+					id: "c",
+					title: "c",
+					content: "c",
+				}) {
+					id
+				}
+
+				b: createOnePost(data: {
+					id: "b",
+					title: "b",
+					content: "b",
+				}) {
+					id
+				}
+			}
+		`,
+		run: func(t *testing.T, client Client, ctx cx) {
+			actual, err := client.Post.FindMany().OrderBy(
+				Post.Title.Order(ASC),
+			).Exec(ctx)
+			if err != nil {
+				t.Fatalf("fail %s", err)
+			}
+
+			expected := []PostModel{{
+				post{
+					ID:      "a",
+					Title:   "a",
+					Content: "a",
+				},
+			}, {
+				post{
+					ID:      "b",
+					Title:   "b",
+					Content: "b",
+				},
+			}, {
+				post{
+					ID:      "c",
+					Title:   "c",
+					Content: "c",
+				},
+			}}
+
+			assert.Equal(t, expected, actual)
+		},
+	}, {
+		name: "order by DESC",
+		// language=GraphQL
+		before: `
+			mutation {
+				a: createOnePost(data: {
+					id: "a",
+					title: "a",
+					content: "a",
+				}) {
+					id
+				}
+
+				c: createOnePost(data: {
+					id: "c",
+					title: "c",
+					content: "c",
+				}) {
+					id
+				}
+
+				b: createOnePost(data: {
+					id: "b",
+					title: "b",
+					content: "b",
+				}) {
+					id
+				}
+			}
+		`,
+		run: func(t *testing.T, client Client, ctx cx) {
+			actual, err := client.Post.FindMany().OrderBy(
+				Post.Title.Order(DESC),
+			).Exec(ctx)
+			if err != nil {
+				t.Fatalf("fail %s", err)
+			}
+
+			expected := []PostModel{{
+				post{
+					ID:      "c",
+					Title:   "c",
+					Content: "c",
+				},
+			}, {
+				post{
+					ID:      "b",
+					Title:   "b",
+					Content: "b",
+				},
+			}, {
+				post{
+					ID:      "a",
+					Title:   "a",
+					Content: "a",
+				},
+			}}
+
+			assert.Equal(t, expected, actual)
+		},
+	}}
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			hooks.Run(t)
+
+			client := NewClient()
+			if err := client.Connect(); err != nil {
+				t.Fatalf("could not connect %s", err)
+				return
+			}
+
+			defer func() {
+				_ = client.Disconnect()
+				// TODO blocked by prisma-engine panicking on disconnect
+				// if err != nil {
+				// 	t.Fatalf("could not disconnect %s", err)
+				// }
+			}()
+
+			ctx := context.Background()
+
+			if tt.before != "" {
+				var response gqlResponse
+				err := client.do(ctx, tt.before, &response)
+				if err != nil {
+					t.Fatalf("could not send mock query %s", err)
+				}
+				if response.Errors != nil {
+					t.Fatalf("mock query has errors %+v", response)
+				}
+			}
+
+			tt.run(t, client, ctx)
+		})
+	}
+}
