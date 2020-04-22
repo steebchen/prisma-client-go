bbcc503ca28c71ad010b7c13dab42483c8b9c521
Luca Steeb <contact@luca-steeb.com>
2020-04-22 14:20:32 +0700

    use nat for docker networking

--- .github/workflows/integration.sh
+++ .github/workflows/integration.sh
@@ -3,7 +3,7 @@
 set -eux
 
 # create network
-docker network create integration-bridge
+docker network create --driver nat integration-bridge
 
 # launch postgres
 docker run --network integration-bridge --name postgres -e POSTGRES_PASSWORD=mysecretpassword -d postgres
