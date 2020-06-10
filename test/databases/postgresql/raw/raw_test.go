1e0b15c96b0bc5e2fcf725f6d086e8c5c66f65e2
Luca Steeb <contact@luca-steeb.com>
2020-06-11 01:17:02 +0700

    test(dbs/raw): add more types to raw tests

--- test/databases/postgresql/raw/raw_test.go
+++ test/databases/postgresql/raw/raw_test.go
@@ -12,8 +12,29 @@ import (
 type cx = context.Context
 type Func func(t *testing.T, client *PrismaClient, ctx cx)
 
+type RawUserModel struct {
+	ID       string  `json:"id"`
+	Email    string  `json:"email"`
+	Username string  `json:"username"`
+	Name     *string `json:"name"`
+	Stuff    *string `json:"stuff"`
+	Str      string  `json:"str"`
+	StrOpt   *string `json:"strOpt"`
+	Int      int     `json:"int"`
+	IntOpt   *int    `json:"intOpt"`
+	Float    string  `json:"float"`
+	FloatOpt *string `json:"floatOpt"`
+	Bool     bool    `json:"bool"`
+	BoolOpt  *bool   `json:"boolOpt"`
+}
+
 func TestRaw(t *testing.T) {
 	t.Parallel()
+
+	strOpt := "strOpt"
+	i := 5
+	f := "5.5"
+	b := false
 
 	tests := []struct {
 		name   string
@@ -28,6 +49,14 @@ func TestRaw(t *testing.T) {
 					id: "id1",
 					email: "email1",
 					username: "a",
+					str: "str",
+					strOpt: "strOpt",
+					int: 5,
+					intOpt: 5,
+					float: 5.5,
+					floatOpt: 5.5,
+					bool: true,
+					boolOpt: false,
 				}) {
 					id
 				}
@@ -38,30 +67,54 @@ func TestRaw(t *testing.T) {
 					id: "id2",
 					email: "email2",
 					username: "b",
+					str: "str",
+					strOpt: "strOpt",
+					int: 5,
+					intOpt: 5,
+					float: 5.5,
+					floatOpt: 5.5,
+					bool: true,
+					boolOpt: false,
 				}) {
 					id
 				}
 			}
 		`},
 		run: func(t *testing.T, client *PrismaClient, ctx cx) {
-			var actual []UserModel
+			var actual []RawUserModel
 			err := client.Raw(`SELECT * FROM "User"`).Exec(ctx, &actual)
 			if err != nil {
 				t.Fatalf("fail %s", err)
 			}
 
-			expected := []UserModel{{
-				RawUser: RawUser{
-					ID:       "id1",
-					Email:    "email1",
-					Username: "a",
-				},
+			strOpt := "strOpt"
+			i := 5
+			f := "5.5"
+			b := false
+			expected := []RawUserModel{{
+				ID:       "id1",
+				Email:    "email1",
+				Username: "a",
+				Str:      "str",
+				StrOpt:   &strOpt,
+				Int:      i,
+				IntOpt:   &i,
+				Float:    f,
+				FloatOpt: &f,
+				Bool:     true,
+				BoolOpt:  &b,
 			}, {
-				RawUser: RawUser{
-					ID:       "id2",
-					Email:    "email2",
-					Username: "b",
-				},
+				ID:       "id2",
+				Email:    "email2",
+				Username: "b",
+				Str:      "str",
+				StrOpt:   &strOpt,
+				Int:      i,
+				IntOpt:   &i,
+				Float:    f,
+				FloatOpt: &f,
+				Bool:     true,
+				BoolOpt:  &b,
 			}}
 
 			assert.Equal(t, expected, actual)
@@ -75,6 +128,14 @@ func TestRaw(t *testing.T) {
 					id: "id1",
 					email: "email1",
 					username: "a",
+					str: "str",
+					strOpt: "strOpt",
+					int: 5,
+					intOpt: 5,
+					float: 5.5,
+					floatOpt: 5.5,
+					bool: true,
+					boolOpt: false,
 				}) {
 					id
 				}
@@ -85,24 +146,38 @@ func TestRaw(t *testing.T) {
 					id: "id2",
 					email: "email2",
 					username: "b",
+					str: "str",
+					strOpt: "strOpt",
+					int: 5,
+					intOpt: 5,
+					float: 5.5,
+					floatOpt: 5.5,
+					bool: true,
+					boolOpt: false,
 				}) {
 					id
 				}
 			}
 		`},
 		run: func(t *testing.T, client *PrismaClient, ctx cx) {
-			var actual []UserModel
+			var actual []RawUserModel
 			err := client.Raw(`SELECT * FROM "User" WHERE id = $1`, "id2").Exec(ctx, &actual)
 			if err != nil {
 				t.Fatalf("fail %s", err)
 			}
 
-			expected := []UserModel{{
-				RawUser: RawUser{
-					ID:       "id2",
-					Email:    "email2",
-					Username: "b",
-				},
+			expected := []RawUserModel{{
+				ID:       "id2",
+				Email:    "email2",
+				Username: "b",
+				Str:      "str",
+				StrOpt:   &strOpt,
+				Int:      i,
+				IntOpt:   &i,
+				Float:    f,
+				FloatOpt: &f,
+				Bool:     true,
+				BoolOpt:  &b,
 			}}
 
 			assert.Equal(t, expected, actual)
@@ -116,6 +191,14 @@ func TestRaw(t *testing.T) {
 					id: "id1",
 					email: "email1",
 					username: "a",
+					str: "str",
+					strOpt: "strOpt",
+					int: 5,
+					intOpt: 5,
+					float: 5.5,
+					floatOpt: 5.5,
+					bool: true,
+					boolOpt: false,
 				}) {
 					id
 				}
@@ -126,24 +209,38 @@ func TestRaw(t *testing.T) {
 					id: "id2",
 					email: "email2",
 					username: "b",
+					str: "str",
+					strOpt: "strOpt",
+					int: 5,
+					intOpt: 5,
+					float: 5.5,
+					floatOpt: 5.5,
+					bool: true,
+					boolOpt: false,
 				}) {
 					id
 				}
 			}
 		`},
 		run: func(t *testing.T, client *PrismaClient, ctx cx) {
-			var actual []UserModel
+			var actual []RawUserModel
 			err := client.Raw(`SELECT * FROM "User" WHERE id = $1 AND email = $2`, "id2", "email2").Exec(ctx, &actual)
 			if err != nil {
 				t.Fatalf("fail %s", err)
 			}
 
-			expected := []UserModel{{
-				RawUser: RawUser{
-					ID:       "id2",
-					Email:    "email2",
-					Username: "b",
-				},
+			expected := []RawUserModel{{
+				ID:       "id2",
+				Email:    "email2",
+				Username: "b",
+				Str:      "str",
+				StrOpt:   &strOpt,
+				Int:      i,
+				IntOpt:   &i,
+				Float:    f,
+				FloatOpt: &f,
+				Bool:     true,
+				BoolOpt:  &b,
 			}}
 
 			assert.Equal(t, expected, actual)
@@ -157,6 +254,14 @@ func TestRaw(t *testing.T) {
 					id: "id1",
 					email: "email1",
 					username: "a",
+					str: "str",
+					strOpt: "strOpt",
+					int: 5,
+					intOpt: 5,
+					float: 5.5,
+					floatOpt: 5.5,
+					bool: true,
+					boolOpt: false,
 				}) {
 					id
 				}
@@ -167,6 +272,14 @@ func TestRaw(t *testing.T) {
 					id: "id2",
 					email: "email2",
 					username: "b",
+					str: "str",
+					strOpt: "strOpt",
+					int: 5,
+					intOpt: 5,
+					float: 5.5,
+					floatOpt: 5.5,
+					bool: true,
+					boolOpt: false,
 				}) {
 					id
 				}
