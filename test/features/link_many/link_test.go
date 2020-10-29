2de54ae975a5bc453e303dbc318e7ed6e51ce8ee
Luca Steeb <contact@luca-steeb.com>
2020-10-29 16:57:50 +0700

    fix(query): link many relations in nested create/update

---
+++ test/features/link_many/link_test.go
@@ -0,0 +1,209 @@
+package linkmany
+
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/prisma/prisma-client-go/test"
+)
+
+type cx = context.Context
+type Func func(t *testing.T, client *PrismaClient, ctx cx)
+
+func str(v string) *string {
+	return &v
+}
+
+func TestRelations(t *testing.T) {
+	t.Parallel()
+
+	tests := []struct {
+		name   string
+		before []string
+		run    Func
+	}{{
+		name: "link many in create",
+		// language=GraphQL
+		before: []string{`
+			mutation {
+				user: createOneUser(data: {
+					id: "unrelated",
+					email: "unrelated",
+					username: "unrelated",
+					name: "unrelated",
+					posts: {
+						create: [{
+							id: "a",
+							title: "common",
+							content: "a",
+						}, {
+							id: "b",
+							title: "common",
+							content: "b",
+						}],
+					},
+				}) {
+					id
+				}
+			}
+		`},
+		run: func(t *testing.T, client *PrismaClient, ctx cx) {
+			created, err := client.User.CreateOne(
+				User.Email.Set("new"),
+				User.Username.Set("new"),
+				User.ID.Set("new"),
+				User.Posts.Link(
+					Post.ID.Equals("a"),
+					Post.ID.Equals("b"),
+				),
+			).Exec(ctx)
+			if err != nil {
+				t.Fatalf("fail %s", err)
+			}
+
+			newUser := InternalUser{
+				ID:       "new",
+				Email:    "new",
+				Username: "new",
+			}
+
+			assert.Equal(t, created, UserModel{InternalUser: newUser})
+
+			actual, err := client.User.FindOne(
+				User.ID.Equals("new"),
+			).With(
+				User.Posts.Fetch().Take(2),
+			).Exec(ctx)
+			if err != nil {
+				t.Fatalf("fail %s", err)
+			}
+
+			authorID := "new"
+			expected := UserModel{
+				InternalUser: newUser,
+				RelationsUser: RelationsUser{
+					Posts: []PostModel{{
+						InternalPost: InternalPost{
+							ID:       "a",
+							Title:    "common",
+							Content:  str("a"),
+							AuthorID: &authorID,
+						},
+					}, {
+						InternalPost: InternalPost{
+							ID:       "b",
+							Title:    "common",
+							Content:  str("b"),
+							AuthorID: &authorID,
+						},
+					}},
+				},
+			}
+
+			assert.Equal(t, expected, actual)
+			assert.Equal(t, 2, len(actual.Posts()))
+		},
+	}, {
+		name: "link many in update",
+		// language=GraphQL
+		before: []string{`
+			mutation {
+				user: createOneUser(data: {
+					id: "new",
+					email: "new",
+					username: "new",
+				}) {
+					id
+				}
+			}
+		`, `
+			mutation {
+				post1: createOnePost(data: {
+					id: "a",
+					title: "common",
+					content: "a",
+				}) {
+					id
+				}
+			}
+		`, `
+			mutation {
+				post2: createOnePost(data: {
+					id: "b",
+					title: "common",
+					content: "b",
+				}) {
+					id
+				}
+			}
+		`},
+		run: func(t *testing.T, client *PrismaClient, ctx cx) {
+			created, err := client.User.FindOne(
+				User.ID.Equals("new"),
+			).Update(
+				User.Posts.Link(
+					Post.ID.Equals("a"),
+					Post.ID.Equals("b"),
+				),
+			).Exec(ctx)
+			if err != nil {
+				t.Fatalf("fail %s", err)
+			}
+
+			newUser := InternalUser{
+				ID:       "new",
+				Email:    "new",
+				Username: "new",
+			}
+
+			assert.Equal(t, created, UserModel{InternalUser: newUser})
+
+			actual, err := client.User.FindOne(
+				User.ID.Equals("new"),
+			).With(
+				User.Posts.Fetch().Take(2),
+			).Exec(ctx)
+			if err != nil {
+				t.Fatalf("fail %s", err)
+			}
+
+			authorID := "new"
+			expected := UserModel{
+				InternalUser: newUser,
+				RelationsUser: RelationsUser{
+					Posts: []PostModel{{
+						InternalPost: InternalPost{
+							ID:       "a",
+							Title:    "common",
+							Content:  str("a"),
+							AuthorID: &authorID,
+						},
+					}, {
+						InternalPost: InternalPost{
+							ID:       "b",
+							Title:    "common",
+							Content:  str("b"),
+							AuthorID: &authorID,
+						},
+					}},
+				},
+			}
+
+			assert.Equal(t, expected, actual)
+			assert.Equal(t, 2, len(actual.Posts()))
+		},
+	}}
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			test.RunSerial(t, []test.Database{test.SQLite, test.MySQL, test.PostgreSQL}, func(t *testing.T, db test.Database, ctx context.Context) {
+				client := NewClient()
+				mockDBName := test.Start(t, db, client.Engine, tt.before)
+				defer test.End(t, db, client.Engine, mockDBName)
+				tt.run(t, client, context.Background())
+			})
+		})
+	}
+}
