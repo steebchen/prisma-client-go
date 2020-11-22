d6df2d3d11a4876e0d79263f226317003dcfd460
Luca Steeb <contact@luca-steeb.com>
2020-11-22 16:08:14 +0700

    feat(mocks): generate typed mocks

--- test/mock/mock_test.go
+++ test/mock/mock_test.go
@@ -2,15 +2,12 @@ package mock
 
 import (
 	"context"
-	"log"
 	"testing"
-	"time"
 
 	"github.com/stretchr/testify/assert"
 )
 
-func TestMock(t *testing.T) {
-	now := time.Now()
+func TestInterfaceMock(t *testing.T) {
 	do := func(ctx context.Context, db *PrismaClient) (UserModel, error) {
 		user, err := db.User.FindOne(User.ID.Equals("foo")).Exec(ctx)
 		if err != nil {
@@ -38,6 +35,58 @@ func TestMock(t *testing.T) {
 	actual, err := do(context.Background(), db)
 	assert.Equal(t, expectedErr, err)
 	assert.Equal(t, expected, actual)
+}
 
-	log.Printf("%s", time.Since(now))
+func TestTypedMock(t *testing.T) {
+	do := func(ctx context.Context, db *PrismaClient) (UserModel, error) {
+		user, err := db.User.FindOne(User.ID.Equals("foo")).Exec(ctx)
+		if err != nil {
+			return UserModel{}, err
+		}
+
+		return user, nil
+	}
+
+	var expectedErr error = nil
+	expected := UserModel{
+		InternalUser: InternalUser{
+			ID:   "123",
+			Name: "foo",
+		},
+		RelationsUser: RelationsUser{},
+	}
+
+	db, mock, ensure := NewMock()
+	defer ensure(t)
+	mock.User.Expect(
+		db.User.FindOne(User.ID.Equals("foo")),
+	).Returns(expected)
+
+	actual, err := do(context.Background(), db)
+	assert.Equal(t, expectedErr, err)
+	assert.Equal(t, expected, actual)
+}
+
+func TestMockError(t *testing.T) {
+	do := func(ctx context.Context, db *PrismaClient) (UserModel, error) {
+		user, err := db.User.FindOne(User.ID.Equals("foo")).Exec(ctx)
+		if err != nil {
+			return UserModel{}, err
+		}
+
+		return user, nil
+	}
+
+	expectedErr := ErrNotFound
+	expected := UserModel{}
+
+	db, mock, ensure := NewMock()
+	defer ensure(t)
+	mock.User.Expect(
+		db.User.FindOne(User.ID.Equals("foo")),
+	).Errors(ErrNotFound)
+
+	actual, err := do(context.Background(), db)
+	assert.Equal(t, expectedErr, err)
+	assert.Equal(t, expected, actual)
 }
