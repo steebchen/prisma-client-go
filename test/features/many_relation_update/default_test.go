4749cea8bcbc10f14dd7c9cb9064daccc0d8e9f6
Luca Steeb <contact@luca-steeb.com>
2021-01-20 01:03:10 +0700

    minor test fixes

--- test/features/many_relation_update/default_test.go
+++ test/features/many_relation_update/default_test.go
@@ -1,22 +1,20 @@
-package composite
+package db
 
 import (
 	"context"
-	"github.com/prisma/prisma-client-go/test"
 	"testing"
+
+	"github.com/prisma/prisma-client-go/test"
 )
 
 func TestFindManyRelationUpdate(t *testing.T) {
-	t.Logf("success (compiling means succeeded)")
-
 	test.RunParallel(t, []test.Database{test.MySQL, test.PostgreSQL, test.SQLite}, func(t *testing.T, db test.Database, ctx context.Context) {
 		client := NewClient()
 		mockDBName := test.Start(t, test.SQLite, client.Engine, []string{})
 		defer test.End(t, test.SQLite, client.Engine, mockDBName)
 
 		_, err := client.Post.FindMany(
-			Post.ID.Equals("ckfxy9dh00003c5rp97l1cica"),
-			//Post.UpdatedAt.Before(timestamp),
+			Post.ID.Equals("non-existing"),
 		).Update(
 			Post.User.Link(User.ID.Equals("123")),
 		).Exec(ctx)
