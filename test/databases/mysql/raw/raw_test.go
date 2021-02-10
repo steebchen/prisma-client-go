2879c17ba2eaac6ac135070d9252027b5aae38af
Luca Steeb <contact@luca-steeb.com>
2021-02-10 20:31:56 +0700

    feat(raw): handle time values
    The query engine expects time parameters in a special json
    syntax because JSON doesn't have a time/date type.

--- test/databases/mysql/raw/raw_test.go
+++ test/databases/mysql/raw/raw_test.go
@@ -3,6 +3,7 @@ package raw
 import (
 	"context"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 
@@ -52,6 +53,8 @@ func TestRaw(t *testing.T) {
 					username: "a",
 					str: "str",
 					strOpt: "strOpt",
+					date: "2020-01-01T00:00:00Z",
+					dateOpt: "2020-01-01T00:00:00Z",
 					int: 5,
 					intOpt: 5,
 					float: 5.5,
@@ -70,6 +73,8 @@ func TestRaw(t *testing.T) {
 					username: "b",
 					str: "str",
 					strOpt: "strOpt",
+					date: "2020-01-01T00:00:00Z",
+					dateOpt: "2020-01-01T00:00:00Z",
 					int: 5,
 					intOpt: 5,
 					float: 5.5,
@@ -126,6 +131,8 @@ func TestRaw(t *testing.T) {
 					username: "a",
 					str: "str",
 					strOpt: "strOpt",
+					date: "2020-01-01T00:00:00Z",
+					dateOpt: "2020-01-01T00:00:00Z",
 					int: 5,
 					intOpt: 5,
 					float: 5.5,
@@ -144,6 +151,8 @@ func TestRaw(t *testing.T) {
 					username: "b",
 					str: "str",
 					strOpt: "strOpt",
+					date: "2020-01-01T00:00:00Z",
+					dateOpt: "2020-01-01T00:00:00Z",
 					int: 5,
 					intOpt: 5,
 					float: 5.5,
@@ -188,6 +197,8 @@ func TestRaw(t *testing.T) {
 					username: "a",
 					str: "str",
 					strOpt: "strOpt",
+					date: "2020-01-01T00:00:00Z",
+					dateOpt: "2020-01-01T00:00:00Z",
 					int: 5,
 					intOpt: 5,
 					float: 5.5,
@@ -206,6 +217,8 @@ func TestRaw(t *testing.T) {
 					username: "b",
 					str: "str",
 					strOpt: "strOpt",
+					date: "2020-01-01T00:00:00Z",
+					dateOpt: "2020-01-01T00:00:00Z",
 					int: 5,
 					intOpt: 5,
 					float: 5.5,
@@ -250,6 +263,8 @@ func TestRaw(t *testing.T) {
 					username: "a",
 					str: "str",
 					strOpt: "strOpt",
+					date: "2020-01-01T00:00:00Z",
+					dateOpt: "2020-01-01T00:00:00Z",
 					int: 5,
 					intOpt: 5,
 					float: 5.5,
@@ -268,6 +283,8 @@ func TestRaw(t *testing.T) {
 					username: "b",
 					str: "str",
 					strOpt: "strOpt",
+					date: "2020-01-01T00:00:00Z",
+					dateOpt: "2020-01-01T00:00:00Z",
 					int: 5,
 					intOpt: 5,
 					float: 5.5,
@@ -293,7 +310,11 @@ func TestRaw(t *testing.T) {
 		name:   "insert into",
 		before: []string{},
 		run: func(t *testing.T, client *PrismaClient, ctx cx) {
-			result, err := client.Prisma.ExecuteRaw("INSERT INTO `User` (`id`, `email`, `username`, `str`, `strOpt`, `int`, `intOpt`, `float`, `floatOpt`, `bool`, `boolOpt`) VALUES(?,?,?,?,?,?,?,?,?,?,?)", "a", "a", "a", "a", "a", 1, 1, 2.0, 2.0, true, false).Exec(ctx)
+			date, err := time.Parse(time.RFC3339, "2020-01-01T00:00:00Z")
+			if err != nil {
+				t.Fatal(err)
+			}
+			result, err := client.Prisma.ExecuteRaw("INSERT INTO `User` (`id`, `email`, `username`, `str`, `strOpt`, `date`, `dateOpt`, `int`, `intOpt`, `float`, `floatOpt`, `bool`, `boolOpt`) VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?)", "a", "a", "a", "a", "a", date, date, 1, 1, 2.0, 2.0, true, false).Exec(ctx)
 			if err != nil {
 				t.Fatalf("fail %s", err)
 			}
@@ -311,6 +332,8 @@ func TestRaw(t *testing.T) {
 					username: "a",
 					str: "str",
 					strOpt: "strOpt",
+					date: "2020-01-01T00:00:00Z",
+					dateOpt: "2020-01-01T00:00:00Z",
 					int: 5,
 					intOpt: 5,
 					float: 5.5,
@@ -336,6 +359,76 @@ func TestRaw(t *testing.T) {
 			}
 
 			assert.Equal(t, 0, result.Count)
+		},
+	}, {
+		name: "raw query with time parameter",
+		// language=GraphQL
+		before: []string{`
+			mutation {
+				result: createOneUser(data: {
+					id: "id1",
+					email: "email1",
+					username: "a",
+					str: "str",
+					strOpt: "strOpt",
+					date: "2010-01-01T00:00:00Z",
+					dateOpt: "2010-01-01T00:00:00Z",
+					int: 5,
+					intOpt: 5,
+					float: 5.5,
+					floatOpt: 5.5,
+					bool: true,
+					boolOpt: false,
+				}) {
+					id
+				}
+			}
+		`, `
+			mutation {
+				result: createOneUser(data: {
+					id: "id2",
+					email: "email2",
+					username: "b",
+					str: "str",
+					strOpt: "strOpt",
+					date: "2020-01-01T00:00:00Z",
+					dateOpt: "2020-01-01T00:00:00Z",
+					int: 5,
+					intOpt: 5,
+					float: 5.5,
+					floatOpt: 5.5,
+					bool: true,
+					boolOpt: false,
+				}) {
+					id
+				}
+			}
+		`},
+		run: func(t *testing.T, client *PrismaClient, ctx cx) {
+			date, err := time.Parse(time.RFC3339, "2020-01-01T00:00:00Z")
+			if err != nil {
+				t.Fatal(err)
+			}
+			var actual []RawUserModel
+			if err := client.Prisma.QueryRaw("SELECT * FROM `User` WHERE date = ?", date).Exec(ctx, &actual); err != nil {
+				t.Fatalf("fail %s", err)
+			}
+
+			expected := []RawUserModel{{
+				ID:       "id2",
+				Email:    "email2",
+				Username: "b",
+				Str:      "str",
+				StrOpt:   &strOpt,
+				Int:      i,
+				IntOpt:   &i,
+				Float:    f,
+				FloatOpt: &f,
+				Bool:     bTrue,
+				BoolOpt:  &bFalse,
+			}}
+
+			assert.Equal(t, expected, actual)
 		},
 	}}
 	for _, tt := range tests {
