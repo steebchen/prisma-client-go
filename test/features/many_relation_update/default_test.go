fa94d61d04045489c8bcbad5753556647d6db487
Luca Steeb <contact@luca-steeb.com>
2021-04-07 23:31:07 +0700

    Update default_test.go

--- test/features/many_relation_update/default_test.go
+++ test/features/many_relation_update/default_test.go
@@ -7,8 +7,9 @@ import (
 	"github.com/prisma/prisma-client-go/test"
 )
 
-func TestFindManyRelationUpdate(t *testing.T) {
+func TestFindManyRelationUpdateLink(t *testing.T) {
 	test.RunParallel(t, []test.Database{test.MySQL, test.PostgreSQL, test.SQLite}, func(t *testing.T, db test.Database, ctx context.Context) {
+		t.Skip() // this currently doesn't work as the QE doesn't support updateMany connect syntax
 		client := NewClient()
 		mockDBName := test.Start(t, test.SQLite, client.Engine, []string{})
 		defer test.End(t, test.SQLite, client.Engine, mockDBName)
@@ -17,6 +18,23 @@ func TestFindManyRelationUpdate(t *testing.T) {
 			Post.ID.Equals("non-existing"),
 		).Update(
 			Post.User.Link(User.ID.Equals("123")),
+		).Exec(ctx)
+		if err != nil {
+			t.Fatal(err)
+		}
+	})
+}
+
+func TestFindManyRelationUpdateScalar(t *testing.T) {
+	test.RunParallel(t, []test.Database{test.MySQL, test.PostgreSQL, test.SQLite}, func(t *testing.T, db test.Database, ctx context.Context) {
+		client := NewClient()
+		mockDBName := test.Start(t, test.SQLite, client.Engine, []string{})
+		defer test.End(t, test.SQLite, client.Engine, mockDBName)
+
+		_, err := client.Post.FindMany(
+			Post.ID.Equals("non-existing"),
+		).Update(
+			Post.UserID.Equals("123"),
 		).Exec(ctx)
 		if err != nil {
 			t.Fatal(err)
