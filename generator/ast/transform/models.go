7abf7d654cd71d3eca23557f6abb7ca24e6bb3d0
Luca Steeb <contact@luca-steeb.com>
2021-08-31 16:45:13 +0700

    feat(ast): introduce ast models

---
+++ generator/ast/transform/models.go
@@ -0,0 +1,38 @@
+package transform
+
+import (
+	"github.com/prisma/prisma-client-go/generator/ast/dmmf"
+	"github.com/prisma/prisma-client-go/generator/types"
+)
+
+type Model struct {
+	Name   types.String `json:"name"`
+	Fields []Field      `json:"fields"`
+
+	// TODO remove this and apply all required data directly to model
+	OldModel dmmf.Model
+}
+
+type Field struct {
+	// TODO re-declare all fields here instead of embedding dmmf.Field
+
+	dmmf.Field
+}
+
+func (r *AST) models() []Model {
+	var models []Model
+	for _, model := range r.dmmf.Datamodel.Models {
+		var fields []Field
+		for _, field := range model.Fields {
+			fields = append(fields, Field{
+				Field: field,
+			})
+		}
+		models = append(models, Model{
+			Name:     model.Name,
+			Fields:   fields,
+			OldModel: model,
+		})
+	}
+	return models
+}
