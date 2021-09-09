d66bb722e969e48f522e7669500128f8ff1e46cc
Luca Steeb <contact@luca-steeb.com>
2021-09-09 16:14:27 +0700

    fix(mock): add ReturnsMany method for list operations (#571)

--- test/mock/mock_test.go
+++ test/mock/mock_test.go
@@ -7,7 +7,7 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestTypedMock(t *testing.T) {
+func TestTypedMockReturns(t *testing.T) {
 	do := func(ctx context.Context, client *PrismaClient) (*UserModel, error) {
 		user, err := client.User.FindUnique(User.ID.Equals("foo")).Exec(ctx)
 		if err != nil {
@@ -31,6 +31,33 @@ func TestTypedMock(t *testing.T) {
 	mock.User.Expect(
 		client.User.FindUnique(User.ID.Equals("foo")),
 	).Returns(*expected)
+
+	actual, err := do(context.Background(), client)
+	assert.Equal(t, expectedErr, err)
+	assert.Equal(t, expected, actual)
+}
+
+func TestTypedMockReturnsMany(t *testing.T) {
+	do := func(ctx context.Context, client *PrismaClient) ([]UserModel, error) {
+		return client.User.FindMany(User.Name.Equals("foo")).Exec(ctx)
+	}
+
+	var expectedErr error = nil
+	expected := []UserModel{
+		{
+			InnerUser: InnerUser{
+				ID:   "123",
+				Name: "foo",
+			},
+			RelationsUser: RelationsUser{},
+		},
+	}
+
+	client, mock, ensure := NewMock()
+	defer ensure(t)
+	mock.User.Expect(
+		client.User.FindMany(User.Name.Equals("foo")),
+	).ReturnsMany(expected)
 
 	actual, err := do(context.Background(), client)
 	assert.Equal(t, expectedErr, err)
