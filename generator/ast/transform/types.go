7a0bf4b097fa77bdaabffa5f71caddc47db52501
Luca Steeb <contact@luca-steeb.com>
2021-09-13 22:39:05 +0700

    feat(scalars): get scalars from dmmf (#574)

--- generator/ast/transform/types.go
+++ generator/ast/transform/types.go
@@ -1,11 +1,32 @@
 package transform
 
 func (r *AST) scalars() []string {
-	return []string{
-		"String",
-		"Bool",
-		"Int",
-		"Float",
-		"DateTime",
+	var scalars []string
+	for _, item := range r.dmmf.Schema.InputObjectTypes.Prisma {
+		for _, field := range item.Fields {
+			for _, input := range field.InputTypes {
+				if input.Location != "scalar" {
+					continue
+				}
+
+				name := input.Type.String()
+
+				var exists bool
+				for _, s := range scalars {
+					// prevent duplicate items
+					if s == name {
+						exists = true
+					}
+				}
+
+				if exists {
+					continue
+				}
+
+				scalars = append(scalars, name)
+			}
+		}
 	}
+
+	return scalars
 }
