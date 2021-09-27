d2d4928026a3dd9db70f0947de6753a63b63ddab
Luca Steeb <contact@luca-steeb.com>
2021-09-27 21:00:55 +0700

    refactor(engine): add qe.go (#595)

--- engine/engine.go
+++ engine/engine.go
@@ -2,17 +2,7 @@ package engine
 
 import (
 	"context"
-	"net/http"
-	"os/exec"
 )
-
-func New(schema string, hasBinaryTargets bool) *QueryEngine {
-	return &QueryEngine{
-		Schema:           schema,
-		hasBinaryTargets: hasBinaryTargets,
-		http:             &http.Client{},
-	}
-}
 
 type Engine interface {
 	Connect() error
@@ -20,31 +10,4 @@ type Engine interface {
 	Do(ctx context.Context, payload interface{}, into interface{}) error
 	Batch(ctx context.Context, payload interface{}, into interface{}) error
 	Name() string
-}
-
-type QueryEngine struct {
-	// cmd holds the prisma binary process
-	cmd *exec.Cmd
-
-	// http is the internal http client
-	http *http.Client
-
-	// url holds the query-engine url
-	url string
-
-	// Schema contains the prisma Schema
-	Schema string
-
-	// hasBinaryTargets can be toggled by generated code from Schema.prisma whether binaryTargets
-	// were specified and thus expects binaries in the local path
-	hasBinaryTargets bool
-}
-
-func (e *QueryEngine) Name() string {
-	return "query-engine"
-}
-
-// deprecated
-func (e *QueryEngine) ReplaceSchema(replace func(schema string) string) {
-	e.Schema = replace(e.Schema)
 }
