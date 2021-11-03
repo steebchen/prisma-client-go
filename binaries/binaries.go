03b9756562a2fd809d5feedbec319849a6a4555c
Luca Steeb <contact@luca-steeb.com>
2021-11-03 23:28:11 +0700

    feat(prisma): upgrade prisma to 3.4.0 (#671)

--- binaries/binaries.go
+++ binaries/binaries.go
@@ -15,11 +15,11 @@ import (
 )
 
 // PrismaVersion is a hardcoded version of the Prisma CLI.
-const PrismaVersion = "3.3.0"
+const PrismaVersion = "3.4.0"
 
 // EngineVersion is a hardcoded version of the Prisma Engine.
 // The versions can be found under https://github.com/prisma/prisma-engines/commits/master
-const EngineVersion = "33838b0f78f1fe9052cf9a00e9761c9dc097a63c"
+const EngineVersion = "1c9fdaa9e2319b814822d6dbfd0a69e1fcc13a85"
 
 // PrismaURL points to an S3 bucket URL where the CLI binaries are stored.
 var PrismaURL = "https://prisma-photongo.s3-eu-west-1.amazonaws.com/%s-%s-%s.gz"
