518d7b7679d6582c00e181f69dc547f14d1c54b5
anson <caoyanbin1993@foxmail.com>
2022-09-18 16:58:34 +0800

    add learn example

---
+++ example/go.mod
@@ -0,0 +1,13 @@
+module demo
+
+go 1.18
+
+require (
+	github.com/iancoleman/strcase v0.0.0-20190422225806-e506e3ef7365
+	github.com/joho/godotenv v1.4.0
+	github.com/prisma/prisma-client-go v0.16.2
+	github.com/shopspring/decimal v1.3.1
+	github.com/takuoki/gocase v1.0.0
+)
+
+replace github.com/prisma/prisma-client-go => ../
---
+++ example/main.go
@@ -0,0 +1,84 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+
+	"demo/db"
+)
+
+func main() {
+	if err := run(); err != nil {
+		panic(err)
+	}
+}
+
+func run() error {
+	client := db.NewClient()
+	if err := client.Prisma.Connect(); err != nil {
+		return err
+	}
+
+	defer func() {
+		if err := client.Prisma.Disconnect(); err != nil {
+			panic(err)
+		}
+	}()
+
+	ctx := context.Background()
+
+	// create a post
+	createdPost, err := client.Post.CreateOne(
+		db.Post.Title.Set("Hi from Prisma!"),
+		db.Post.Published.Set(true),
+		db.Post.Desc.Set("Prisma is a database toolkit and makes databases easy."),
+	).Exec(ctx)
+	if err != nil {
+		return err
+	}
+
+	result, _ := json.MarshalIndent(createdPost, "", "  ")
+	fmt.Printf("created post: %s\n", result)
+
+	// find a single post
+	post, err := client.Post.FindUnique(
+		db.Post.ID.Equals(createdPost.ID),
+	).Exec(ctx)
+	if err != nil {
+		return err
+	}
+
+	result, _ = json.MarshalIndent(post, "", "  ")
+	fmt.Printf("post: %s\n", result)
+
+	// for optional/nullable values, you need to check the function and create two return values
+	// `desc` is a string, and `ok` is a bool whether the record is null or not. If it's null,
+	// `ok` is false, and `desc` will default to Go's default values; in this case an empty string (""). Otherwise,
+	// `ok` is true and `desc` will be "my description".
+	desc, ok := post.Desc()
+	if !ok {
+		return fmt.Errorf("post's description is null")
+	}
+
+	fmt.Printf("The posts's description is: %s\n", desc)
+
+	createUserA := client.Post.CreateOne(
+		db.Post.Title.Set("2"),
+		db.Post.Published.Set(true),
+		db.Post.Desc.Set("222."),
+	).Tx()
+
+	createUserB := client.Post.CreateOne(
+		db.Post.Title.Set("3"),
+		db.Post.Published.Set(true),
+		db.Post.Desc.Set("222."),
+	).Tx()
+
+	tx := client.Prisma.Transaction(createUserA, createUserB)
+	if err := tx.Exec(ctx); err != nil {
+		panic(err)
+	}
+
+	return nil
+}
