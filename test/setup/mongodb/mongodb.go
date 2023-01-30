7b3822c1a5764966d5a89a6d0a646c86489e292d
steebchen <contact@luca-steeb.com>
2023-01-30 20:39:51 +0800

    refactor(tests): use docker-compose for test deps
    also adds custom dockerfile for mongodb
    see https://github.com/prisma/prisma/discussions/17613

--- test/setup/mongodb/mongodb.go
+++ test/setup/mongodb/mongodb.go
@@ -2,15 +2,10 @@ package mongodb
 
 import (
 	"fmt"
-	"log"
 	"testing"
 
-	"github.com/prisma/prisma-client-go/test/cmd"
 	"github.com/prisma/prisma-client-go/test/setup"
 )
-
-const containerName = "go-client-mongodb"
-const image = "prismagraphql/mongo-single-replica:4.4.3-bionic"
 
 var MongoDB = &mongoDB{}
 
@@ -29,19 +24,3 @@ func (*mongoDB) SetupDatabase(t *testing.T) string {
 }
 
 func (*mongoDB) TeardownDatabase(t *testing.T, mockDB string) {}
-
-func (db *mongoDB) Setup() {
-	if err := cmd.Run("docker", "run", "--name", containerName, "-p", "27016:27016", "-e", "MONGO_INITDB_ROOT_USERNAME=prisma", "-e", "MONGO_INITDB_ROOT_PASSWORD=pw", "-e", "MONGO_PORT=27016", "-d", image); err != nil {
-		panic(err)
-	}
-}
-
-func (db *mongoDB) Teardown() {
-	if err := cmd.Run("docker", "stop", containerName); err != nil {
-		log.Println(err)
-	}
-
-	if err := cmd.Run("docker", "rm", containerName, "--force"); err != nil {
-		log.Println(err)
-	}
-}
