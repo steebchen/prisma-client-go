d69488b7cfc805303bf0646e2e70184e4548f4f1
Luca Steeb <contact@luca-steeb.com>
2023-02-08 00:24:48 +0800

    chore(prisma): upgrade to prisma 4.7.1 (#805)

--- binaries/binaries.go
+++ binaries/binaries.go
@@ -15,11 +15,11 @@ import (
 )
 
 // PrismaVersion is a hardcoded version of the Prisma CLI.
-const PrismaVersion = "4.1.1"
+const PrismaVersion = "4.7.1"
 
 // EngineVersion is a hardcoded version of the Prisma Engine.
 // The versions can be found under https://github.com/prisma/prisma-engines/commits/master
-const EngineVersion = "8d8414deb360336e4698a65aa45a1fbaf1ce13d8"
+const EngineVersion = "272861e07ab64f234d3ffc4094e32bd61775599c"
 
 // PrismaURL points to an S3 bucket URL where the CLI binaries are stored.
 var PrismaURL = "https://packaged-cli.prisma.sh/%s-%s-%s-x64.gz"
