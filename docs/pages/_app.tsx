967017ecb1294240665e30464f8f21e3c84bdd6f
steebchen <contact@luca-steeb.com>
2023-05-24 15:32:34 +0100

    feat(docs): enable vercel anonymous analytics

--- docs/pages/_app.tsx
+++ docs/pages/_app.tsx
@@ -1,5 +1,11 @@
+import { Analytics } from '@vercel/analytics/react'
 import './overrides.css'
 
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics/>
+    </>
+  )
 }
