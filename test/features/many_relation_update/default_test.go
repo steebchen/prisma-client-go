da4c8830f16c0d9eecc5de17cbeb3314051ddafc
Luca Steeb <contact@luca-steeb.com>
2023-06-10 22:01:03 +0100

    test(update): add test for linking in updateMany (#268)

---
+++ test/features/many_relation_update/default_test.go
@@ -0,0 +1,43 @@
+package db
+
+import (
+	"context"
+	"testing"
+
+	"github.com/prisma/prisma-client-go/test"
+)
+
+func TestFindManyRelationUpdateLink(t *testing.T) {
+	test.RunSerial(t, []test.Database{test.MySQL, test.PostgreSQL, test.SQLite}, func(t *testing.T, db test.Database, ctx context.Context) {
+		t.Skip("skip until #401 is implemented")
+		client := NewClient()
+		mockDBName := test.Start(t, test.SQLite, client.Engine, []string{})
+		defer test.End(t, test.SQLite, client.Engine, mockDBName)
+
+		_, err := client.Post.FindMany(
+			Post.ID.Equals("non-existing"),
+		).Update(
+			Post.User.Link(User.ID.Equals("123")),
+		).Exec(ctx)
+		if err != nil {
+			t.Fatal(err)
+		}
+	})
+}
+
+func TestFindManyRelationUpdateScalar(t *testing.T) {
+	test.RunSerial(t, []test.Database{test.MySQL, test.PostgreSQL, test.SQLite}, func(t *testing.T, db test.Database, ctx context.Context) {
+		client := NewClient()
+		mockDBName := test.Start(t, test.SQLite, client.Engine, []string{})
+		defer test.End(t, test.SQLite, client.Engine, mockDBName)
+
+		_, err := client.Post.FindMany(
+			Post.ID.Equals("non-existing"),
+		).Update(
+			Post.UserID.Set("123"),
+		).Exec(ctx)
+		if err != nil {
+			t.Fatal(err)
+		}
+	})
+}
