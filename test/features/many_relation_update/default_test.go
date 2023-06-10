680df07485fd6410db9eabfd6f19c1785e323381
steebchen <contact@luca-steeb.com>
2023-06-10 21:14:37 +0100

    fix tests

--- test/features/many_relation_update/default_test.go
+++ test/features/many_relation_update/default_test.go
@@ -9,7 +9,6 @@ import (
 
 func TestFindManyRelationUpdateLink(t *testing.T) {
 	test.RunSerial(t, []test.Database{test.MySQL, test.PostgreSQL, test.SQLite}, func(t *testing.T, db test.Database, ctx context.Context) {
-		t.Skip() // this currently doesn't work as the QE doesn't support updateMany connect syntax
 		client := NewClient()
 		mockDBName := test.Start(t, test.SQLite, client.Engine, []string{})
 		defer test.End(t, test.SQLite, client.Engine, mockDBName)
@@ -34,7 +33,7 @@ func TestFindManyRelationUpdateScalar(t *testing.T) {
 		_, err := client.Post.FindMany(
 			Post.ID.Equals("non-existing"),
 		).Update(
-			Post.UserID.Equals("123"),
+			Post.UserID.Set("123"),
 		).Exec(ctx)
 		if err != nil {
 			t.Fatal(err)
