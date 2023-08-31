83572d2ac965821cd519c31b4c0a08839b5fe4a7
steebchen <contact@luca-steeb.com>
2023-08-31 23:10:45 +0200

    refactor(test/composite-id): use table test

--- test/features/composite_id/composite_id_test.go
+++ test/features/composite_id/composite_id_test.go
@@ -7,30 +7,46 @@ import (
 	"github.com/steebchen/prisma-client-go/test"
 )
 
-// TODO add test for find unique by composite id
+type cx = context.Context
+type Func func(t *testing.T, client *PrismaClient, ctx cx)
 
 func TestCompositeID(t *testing.T) {
-	test.RunParallel(t, []test.Database{test.MySQL, test.PostgreSQL}, func(t *testing.T, db test.Database, ctx context.Context) {
-		client := NewClient()
+	t.Parallel()
 
-		mockDB := test.Start(t, db, client.Engine, []string{})
-		defer test.End(t, db, client.Engine, mockDB)
+	tests := []struct {
+		name   string
+		before []string
+		run    Func
+	}{{
+		name: "create",
+		run: func(t *testing.T, client *PrismaClient, ctx cx) {
+			_, err := client.Repository.CreateOne(
+				Repository.PlatformID.Set("test"),
+				Repository.PlatformKind.Set("test"),
+			).Exec(ctx)
+			if err != nil {
+				t.Fatalf("fail %s", err)
+			}
 
-		_, err := client.Repository.CreateOne(
-			Repository.PlatformID.Set("test"),
-			Repository.PlatformKind.Set("test"),
-		).Exec(ctx)
-		if err != nil {
-			t.Fatalf("fail %s", err)
-		}
-
-		_, err = client.RepositoryOrganization.CreateOne(
-			RepositoryOrganization.PlatformID.Set("test"),
-			RepositoryOrganization.PlatformKind.Set("test"),
-			RepositoryOrganization.Name.Set("test"),
-		).Exec(ctx)
-		if err != nil {
-			t.Fatalf("fail %s", err)
-		}
-	})
+			_, err = client.RepositoryOrganization.CreateOne(
+				RepositoryOrganization.PlatformID.Set("test"),
+				RepositoryOrganization.PlatformKind.Set("test"),
+				RepositoryOrganization.Name.Set("test"),
+			).Exec(ctx)
+			if err != nil {
+				t.Fatalf("fail %s", err)
+			}
+		},
+	}}
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			test.RunSerial(t, []test.Database{test.MySQL, test.PostgreSQL, test.SQLite}, func(t *testing.T, db test.Database, ctx context.Context) {
+				client := NewClient()
+				mockDBName := test.Start(t, db, client.Engine, tt.before)
+				defer test.End(t, db, client.Engine, mockDBName)
+				tt.run(t, client, context.Background())
+			})
+		})
+	}
 }
