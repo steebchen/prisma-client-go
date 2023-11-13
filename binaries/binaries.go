bfc77b40902aea5febc1d796ed69a326d1dd9e1c
Luca Steeb <contact@luca-steeb.com>
2023-11-13 18:56:57 +0700

    refactor(binaries): move version definition to version.go (#1105)

--- binaries/binaries.go
+++ binaries/binaries.go
@@ -14,13 +14,6 @@ import (
 	"github.com/steebchen/prisma-client-go/logger"
 )
 
-// PrismaVersion is a hardcoded version of the Prisma CLI.
-const PrismaVersion = "5.5.2"
-
-// EngineVersion is a hardcoded version of the Prisma Engine.
-// The versions can be found under https://github.com/prisma/prisma-engines/commits/main
-const EngineVersion = "aebc046ce8b88ebbcb45efe31cbe7d06fd6abc0a"
-
 // PrismaURL points to an S3 bucket URL where the CLI binaries are stored.
 var PrismaURL = "https://packaged-cli.prisma.sh/%s-%s-%s-%s.gz"
 
