a0829ef1f0e689b2195209c8213e5960e1a923c4
Luca Steeb <contact@luca-steeb.com>
2023-11-15 13:56:34 +0700

    chore(prisma): upgrade prisma to v5.6.0 (#1106)
    * chore(prisma): upgrade prisma to v5.6.0
    
    * remove unquoting as it was fixed in the query engine
    
    ---------
    
    Co-authored-by: steebchen <steebchen@users.noreply.github.com>

--- binaries/version.go
+++ binaries/version.go
@@ -1,8 +1,8 @@
 package binaries
 
 // PrismaVersion is a hardcoded version of the Prisma CLI.
-const PrismaVersion = "5.5.2"
+const PrismaVersion = "5.6.0"
 
 // EngineVersion is a hardcoded version of the Prisma Engine.
 // The versions can be found under https://github.com/prisma/prisma-engines/commits/main
-const EngineVersion = "aebc046ce8b88ebbcb45efe31cbe7d06fd6abc0a"
+const EngineVersion = "e95e739751f42d8ca026f6b910f5a2dc5adeaeee"
