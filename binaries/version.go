d7503a517a3e6918d2892569cc4073341b99c326
Luca Steeb <contact@luca-steeb.com>
2024-04-04 00:35:41 +0700

    chore(prisma): upgrade prisma to v5.12.0 (#1232)

--- binaries/version.go
+++ binaries/version.go
@@ -1,8 +1,8 @@
 package binaries
 
 // PrismaVersion is a hardcoded version of the Prisma CLI.
-const PrismaVersion = "5.11.0"
+const PrismaVersion = "5.12.0"
 
 // EngineVersion is a hardcoded version of the Prisma Engine.
 // The versions can be found under https://github.com/prisma/prisma-engines/commits/main
-const EngineVersion = "efd2449663b3d73d637ea1fd226bafbcf45b3102"
+const EngineVersion = "473ed3124229e22d881cb7addf559799debae1ab"
