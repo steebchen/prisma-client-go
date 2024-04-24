a03fd3a7776018198c0f1083daa51acdc4bd994d
steebchen <5013932+steebchen@users.noreply.github.com>
2024-04-24 00:05:29 +0000

    chore(prisma): upgrade prisma to v5.13.0

--- binaries/version.go
+++ binaries/version.go
@@ -1,8 +1,8 @@
 package binaries
 
 // PrismaVersion is a hardcoded version of the Prisma CLI.
-const PrismaVersion = "5.12.1"
+const PrismaVersion = "5.13.0"
 
 // EngineVersion is a hardcoded version of the Prisma Engine.
 // The versions can be found under https://github.com/prisma/prisma-engines/commits/main
-const EngineVersion = "473ed3124229e22d881cb7addf559799debae1ab"
+const EngineVersion = "b9a39a7ee606c28e3455d0fd60e78c3ba82b1a2b"
