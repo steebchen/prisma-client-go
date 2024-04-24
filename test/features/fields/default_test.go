50a8e0fe7024cbe375b264cdc07f4e9bdaf7ebc5
steebchen <contact@luca-steeb.com>
2024-04-24 17:53:23 +0100

    feat(builder): add select option

--- test/features/fields/default_test.go
+++ test/features/fields/default_test.go
@@ -19,7 +19,7 @@ func i(v int) *int {
 	return &v
 }
 
-func TestOmit(t *testing.T) {
+func TestFields(t *testing.T) {
 	tests := []struct {
 		name   string
 		before []string
@@ -76,6 +76,59 @@ func TestOmit(t *testing.T) {
 				User.Password.Field(),
 				User.Age.Field(),
 				User.Name.Field(),
+			).Exec(ctx)
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			massert.Equal(t, expected, oneUser)
+		},
+	}, {
+		name:   "select",
+		before: nil,
+		run: func(t *testing.T, client *PrismaClient, ctx cx) {
+			user, err := client.User.CreateOne(
+				User.Name.Set("a"),
+				User.Keep.Set("keep"),
+				User.ID.Set("123"),
+				User.Password.Set("password"),
+				User.Age.Set(20),
+			).Exec(ctx)
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			massert.Equal(t, &UserModel{
+				InnerUser: InnerUser{
+					ID:       "123",
+					Name:     "a",
+					Keep:     "keep",
+					Password: str("password"),
+					Age:      i(20),
+				},
+			}, user)
+
+			expected := &UserModel{
+				InnerUser: InnerUser{
+					Keep: "keep",
+				},
+			}
+
+			users, err := client.User.FindMany(
+				User.Name.Equals("a"),
+			).Select(
+				User.Keep.Field(),
+			).Exec(ctx)
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			massert.Equal(t, []UserModel{*expected}, users)
+
+			oneUser, err := client.User.FindUnique(
+				User.ID.Equals("123"),
+			).Select(
+				User.Keep.Field(),
 			).Exec(ctx)
 			if err != nil {
 				t.Fatal(err)
