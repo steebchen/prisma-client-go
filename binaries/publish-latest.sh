ad36828b00b5bb45b2851b971017cd0c0616f19d
steebchen <contact@luca-steeb.com>
2024-05-10 16:19:52 +0100

    chore(publish-cli): retry latest release command

--- binaries/publish-latest.sh
+++ binaries/publish-latest.sh
@@ -3,5 +3,12 @@
 set -eux
 
 v=$(curl -s https://api.github.com/repos/prisma/prisma/releases/latest | jq -r .tag_name)
+for i in {1..20}; do
+  if [ "$v" != "null" ]; then
+    break
+  fi
+  v=$(curl -s https://api.github.com/repos/prisma/prisma/releases/latest | jq -r .tag_name)
+  sleep $i
+done
 
 sh publish.sh "$v"
