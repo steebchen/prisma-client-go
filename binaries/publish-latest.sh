43179727bf9b8c7d8eecd2eb378be30566038be6
Luca Steeb <contact@luca-steeb.com>
2024-05-17 14:25:09 +0100

    chore(publish-cli): fail if latest version could never be found (#1279)

--- binaries/publish-latest.sh
+++ binaries/publish-latest.sh
@@ -2,13 +2,22 @@
 
 set -eux
 
-v=$(curl -s https://api.github.com/repos/prisma/prisma/releases/latest | jq -r .tag_name)
+url="https://api.github.com/repos/prisma/prisma/releases/latest"
+
+v=$(curl -s "$url" | jq -r .tag_name)
 for i in {1..20}; do
   if [ "$v" != "null" ]; then
     break
   fi
-  v=$(curl -s https://api.github.com/repos/prisma/prisma/releases/latest | jq -r .tag_name)
+  v=$(curl -s "$url" | jq -r .tag_name)
   sleep $i
 done
+
+if [ "$v" = "null" ]; then
+  echo "Could not find latest version"
+  echo "full response:"
+  curl -s "$url"
+  exit 1
+fi
 
 sh publish.sh "$v"
