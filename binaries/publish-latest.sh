49515979d80e44f21298645edb0e6815d3e9041f
Luca Steeb <contact@luca-steeb.com>
2024-06-07 14:57:10 +0200

    chore(cli): authenticate GH API request via GitHub token (#1290)

--- binaries/publish-latest.sh
+++ binaries/publish-latest.sh
@@ -4,19 +4,19 @@ set -eux
 
 url="https://api.github.com/repos/prisma/prisma/releases/latest"
 
-v=$(curl -s "$url" | jq -r .tag_name)
+v=$(curl -s "$url" -H 'Authorization: Bearer $GH_TOKEN' | jq -r .tag_name)
 for i in {1..20}; do
   if [ "$v" != "null" ]; then
     break
   fi
-  v=$(curl -s "$url" | jq -r .tag_name)
+  v=$(curl -s "$url" -H 'Authorization: Bearer $GH_TOKEN' | jq -r .tag_name)
   sleep $i
 done
 
 if [ "$v" = "null" ]; then
   echo "Could not find latest version"
   echo "full response:"
-  curl -s "$url"
+  curl -s "$url" -H 'Authorization: Bearer $GH_TOKEN'
   exit 1
 fi
 
