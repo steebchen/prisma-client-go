1c6b5093f422768dee927641904efbadfeedee29
steebchen <5013932+steebchen@users.noreply.github.com>
2024-08-27 15:52:28 +0000

    5.19.0

--- binaries/version.go
+++ binaries/version.go
@@ -1,8 +1,8 @@
 package binaries
 
 // PrismaVersion is a hardcoded version of the Prisma CLI.
-const PrismaVersion = "5.18.0"
+const PrismaVersion = "5.19.0"
 
 // EngineVersion is a hardcoded version of the Prisma Engine.
 // The versions can be found under https://github.com/prisma/prisma-engines/commits/main
-const EngineVersion = "4c784e32044a8a016d99474bd02a3b6123742169"
+const EngineVersion = "5fe21811a6ba0b952a3bc71400666511fe3b902f"
