a77e31d26e042327779eecdff519b05a9269f868
steebchen <contact@luca-steeb.com>
2024-09-02 20:44:14 +0100

    refactor(binaries): adapt binary publish fetch script

--- binaries/publish-latest.sh
+++ binaries/publish-latest.sh
@@ -4,13 +4,14 @@ set -eux
 
 url="https://api.github.com/repos/prisma/prisma/releases/latest"
 
-v=$(curl -s "$url" -H "Authorization: Bearer $GH_TOKEN" | jq -r .tag_name)
-for i in {1..20}; do
+i=1
+while [ $i -le 20 ]; do
+  v=$(curl -s "$url" -H "Authorization: Bearer $GH_TOKEN" | jq -r .tag_name)
   if [ "$v" != "null" ]; then
     break
   fi
-  v=$(curl -s "$url" -H "Authorization: Bearer $GH_TOKEN" | jq -r .tag_name)
   sleep $i
+  i=$((i+1))
 done
 
 if [ "$v" = "null" ]; then
