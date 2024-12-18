f98e78642c008c52cad2a3a6cc60642478f0386c
steebchen <5013932+steebchen@users.noreply.github.com>
2024-12-18 00:07:08 +0000

    chore(prisma): upgrade prisma to v6.1.0

--- binaries/version.go
+++ binaries/version.go
@@ -1,8 +1,8 @@
 package binaries
 
 // PrismaVersion is a hardcoded version of the Prisma CLI.
-const PrismaVersion = "6.0.1"
+const PrismaVersion = "6.1.0"
 
 // EngineVersion is a hardcoded version of the Prisma Engine.
 // The versions can be found under https://github.com/prisma/prisma-engines/commits/main
-const EngineVersion = "5dbef10bdbfb579e07d35cc85fb1518d357cb99e"
+const EngineVersion = "11f085a2012c0f4778414c8db2651556ee0ef959"
